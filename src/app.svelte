302221b9ca5c1fb18fea85f1792f2247f14348e8
Grilloz <grillo.davide@gmail.com>
2020-04-15 18:18:24 +0200

    App added

---
+++ src/app.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  //$color: red;
+  h1 {
+    text-align: center;
+  }
+  span {
+    color: green;
+  }
+</style>
+
+<h1>Hello <span>{name}</span>!</h1>
